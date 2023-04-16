e67dd5a1e3a98bc6d5aa8d2b77cd31d53758c223
James Storm <jamesstorm@gmail.com>
2023-04-16 15:59:37 -0400

    rust install clean-up

--- rust-debian.sh
+++ rust-debian.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl https://sh.rustup.rs >./installrust.sh
+chmod +x installrust.sh
+./installrust.sh -y
+rm installrust.sh
