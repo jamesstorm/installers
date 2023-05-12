c7075714545d011e28c746cf1e12ce6090a4d8c2
James Storm <jamesstorm@gmail.com>
2023-05-12 11:38:32 -0400

    lazy things

--- lazy.sh
+++ lazy.sh
@@ -2,17 +2,16 @@
 #
 #
 
+backupsuffix=$(date +%s)
 
 # required
-mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.config/nvim ~/.config/nvim.${backupsuffix}
 
 # optional but recommended
-mv ~/.local/share/nvim ~/.local/share/nvim.bak
-mv ~/.local/state/nvim ~/.local/state/nvim.bak
-mv ~/.cache/nvim ~/.cache/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.${backupsuffix}
+mv ~/.local/state/nvim ~/.local/state/nvim.${backupsuffix}
+mv ~/.cache/nvim ~/.cache/nvim.${backupsuffix}
 
-
-git clone https://github.com/LazyVim/starter ~/.config/nvim 
-
+git clone https://github.com/LazyVim/starter ~/.config/nvim
 
 rm -rf ~/.config/nvim/.git
