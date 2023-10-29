660f5d410c697496b105f72b39f03a8065fdf63c
James Storm <jamesstorm@gmail.com>
2023-10-28 21:45:21 -0400

    foo

--- plex_container_setup.sh
+++ plex_container_setup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # fix the slow login issue
 
 systemctl mask systemd-logind
@@ -8,7 +7,9 @@ pam-auth-update >deselect "Register user sessions in the systemd control group h
 # fix ping cap_net thing
 sudo setcap cap_net_raw+ep /bin/ping
 
-apt install tmux zsh sudo python3-pip ansible-core gh nfs-common -y
+apt update
+apt upgrade -y
+apt install dirmngr ca-certificates software-properties-common apt-transport-https tmux zsh sudo python3-pip ansible-core gh nfs-common curl -y
 groupadd --gid 3000 mediausers
 useradd --uid 3000 -m mediauser
 
@@ -18,3 +19,9 @@ echo "Enter a password for mediauser"
 passwd mediauser
 mkdir /mnt/mediashare
 echo "192.168.5.201:/mnt/mr_pool/media /mnt/mediashare nfs defaults 0 0" >>/etc/fstab
+
+curl -sS https://downloads.plex.tv/plex-keys/PlexSign.key | gpg --dearmor | tee /usr/share/keyrings/plex.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/plex.gpg] https://downloads.plex.tv/repo/deb public main" >/etc/apt/sources.list.d/plexmediaserver.list
+apt update
+
+apt update && apt install plexmediaserver -y
