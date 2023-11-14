aa98cc4b277341c2d0b1d55cba6908536ad433b4
James Storm <jamesstorm@gmail.com>
2023-11-14 10:36:07 -0500

    Update user_home_setup.sh
    tweaks

--- user_home_setup.sh
+++ user_home_setup.sh
@@ -6,21 +6,27 @@ mkdir ~/.ssh
 
 
 # OHMYZSH AND POWERLINE10K
+
+rm -rf ~/.oh-my-zsh ~/powerlevel10k 
+
+
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # MY DOTFILES
-git clone https://github.com/jamesstorm/dotfiles ~/dotfiles
-ls -al ~/dotfiles
+rm -rf ~/dotfiles
 rm -rf ~/.zshrc
 rm -rf ~/.tmux.conf
-rm -rf ~/.p10k.sh
+rm -rf ~/.p10k.zsh
+rm -rf ~/.gitconfig
+git clone https://github.com/jamesstorm/dotfiles ~/dotfiles
 ln ~/dotfiles/.zshrc ~/.zshrc
 ln ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln ~/dotfiles/.p10k.zsh ~/.p10k.zsh
-ln ~/dotfiles/.gitconfig ~/.gitconfig
+#ln ~/dotfiles/.gitconfig ~/.gitconfig
 # TMUX POWERLINE
+rm -rf ~/.config/tmux/tmux-powerline
 git clone https://github.com/erikw/tmux-powerline.git ~/.config/tmux/tmux-powerline
 
 # MAKE SURE THE USER OWNS ALL THE THINGS IN THEIR HOME
@@ -28,4 +34,4 @@ git clone https://github.com/erikw/tmux-powerline.git ~/.config/tmux/tmux-powerl
 
 
 
-chsh -s /bin/zsh
+#chsh -s /bin/zsh
