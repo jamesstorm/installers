e96d5fcb5517169378b3e5b124e61f3289835752
James Storm <jamesstorm@gmail.com>
2024-07-23 17:07:00 -0400

    update PATH

--- neovim-tar.sh
+++ neovim-tar.sh
@@ -4,4 +4,4 @@ sudo rm -rf /usr/bin/nvim
 sudo rm -rf /opt/nvim-linux64
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 sudo rm nvim-linux64.tar.gz 
-
+echo "PATH=/opt/nvim-linux64/bin:$PATH" >> ~/.zshrc
