f493763df73317c5c21f839cb1a8963f179ab902
James Storm <jamesstorm@gmail.com>
2024-07-29 09:14:35 -0400

    brew things

--- workstation.sh
+++ workstation.sh
@@ -16,51 +16,44 @@ apt-get update
 apt-get upgrade -y
 
 apt-get install \
-	nala \
-	zsh \
-	wget \
-	sudo \
-	git \
-	software-properties-common \
-	gnupg \
-	ssh \
-	python3-venv \
-	apt-transport-https \
-	ca-certificates \
-	software-properties-common \
-	curl \
-	wget \
-	screenfetch \
-	python3-pip \
-	python3-venv \
-	gnupg \
-	lsb-release \
-	openssh-server \
-	tmux \
-	npm \
-	htop \
-	file \
-	jq \
-	-y
+  nala \
+  zsh \
+  wget \
+  sudo \
+  git \
+  software-properties-common \
+  gnupg \
+  ssh \
+  python3-venv \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  wget \
+  screenfetch \
+  python3-pip \
+  python3-venv \
+  gnupg \
+  lsb-release \
+  openssh-server \
+  tmux
+
+npm \
+  htop \
+  file \
+  jq \
+  -y
 apt-get upgrade -y
 
 # GitHub CLI
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
-	sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
-	sudo apt update &&
-	sudo apt install gh -y
+  sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+  sudo apt update &&
+  sudo apt install gh -y
 
-# NEOVIM stable
-wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-mv squashfs-root /nvim-squashfs-root
-ln -s /nvim-squashfs-root/AppRun /usr/bin/nvim
-rm nvim.appimage
-# INSTALL PACKER FOR NEOVIM
-git clone --depth 1 https://github.com/wbthomason/packer.nvim /home/${user}/.local/share/nvim/site/pack/packer/start/packer.nvim
+# Neovim and Lazy
+./neovim-tar.sh
 ./lazy.sh
 
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
@@ -82,8 +75,15 @@ ln /home/${user}/dotfiles/.zshrc /home/${user}/.zshrc
 ln /home/${user}/dotfiles/.tmux.conf /home/${user}/.tmux.conf
 ln /home/${user}/dotfiles/.p10k.zsh /home/${user}/.p10k.zsh
 ln /home/${user}/dotfiles/.gitconfig /home/${user}/.gitconfig
+
 # TMUX POWERLINE
 git clone https://github.com/erikw/tmux-powerline.git /home/${user}/.config/tmux/tmux-powerline
+
+#brew
+./brew.sh
+
+# Some brew things
+brew install fzf bat dust tldr
 
 # MAKE SURE THE USER OWNS ALL THE THINGS IN THEIR HOME
 chown -R ${user}:${user} /home/${user}
@@ -101,10 +101,8 @@ echo "PATH=/squashfs-root/usr/bin:/home/${user}/.local/bin:$PATH" >>/home/${user
 # Set the timezone
 ln -sf /usr/share/zoneinfo/America/Toronto /etc/localtime
 
-chsh --shell /bin/zsh james
+chsh --shell /bin/zsh ${user}
 
 # Font and Gnome Terminal settings
-#
-
 wget -P /home/${user}/.local/share/fonts/ ${fonturl}
 dconf load /org/gnome/terminal/ </home/${user}/dotfiles/gnome_terminal_settings_backup.txt
