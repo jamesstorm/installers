2120f966605befc492921d7c96c88a08feb8aac0
James Storm <jamesstorm@gmail.com>
2024-09-02 10:23:59 -0400

    config managed by stow now

--- shell.sh
+++ shell.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Move these to a conf file later
-DOTFILES_DIR=$HOME/dotfiles
+DOTFILES_DIR=$HOME/dots
 INSTALLERS_DIR=$HOME/installers
-OMP_ISTALL_DIR=$HOME/.local/bin
-OMP_CONFIG_DIR=$XDG_CONFIG_HOME/ohmyposh
 
 source $INSTALLERS_DIR/functions.sh
 
@@ -16,36 +14,36 @@ DEBUG=1
 # This needs to happen first so we have our environment
 # in scenarios where this is the first run on a fres
 # system
-link_dotfile $DOTFILES_DIR/zsh/zshenv $HOME .zshenv
+stow -d $DOTFILES_DIR zsh
 source $HOME/.zshenv
-echo "ZDOTDIR = $ZDOTDIR"
-link_dotfile $DOTFILES_DIR/zsh/.zshrc $ZDOTDIR .zshrc
-link_dotfile $DOTFILES_DIR/zsh/aliases.zsh $ZDOTDIR aliases.zsh
-link_dotfile $DOTFILES_DIR/zsh/options.zsh $ZDOTDIR options.zsh
 
 # git config
-link_dotfile $DOTFILES_DIR/.gitconfig $HOME .gitconfig
+stow -d $DOTFILES_DIR git
 
 # install nvim
 source $INSTALLERS_DIR/neovim-local.sh
+
 # install lazy
 source $INSTALLERS_DIR/lazy.sh
-# install nvim config
-link_dotfile $DOTFILES_DIR/colorscheme.lua $XDG_CONFIG_HOME/nvim/lua/plugins colorscheme.lua
+
+# nvim/lazy config
+stow -d $DOTFILES_DIR nvim
 
 ## OHMYPOSH INSTALL
 source $INSTALLERS_DIR/ohmyposh.sh
 
 ## OMYMPOSH  CONFIG
-link_dotfile $DOTFILES_DIR/omp.toml $OMP_CONFIG_DIR omp.toml
+stow -d $DOTFILES_DIR ohmyposh
 
 ## ZSH_AUTOSUGGESTIONS
-remkdir $ZDOTDIR/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZDOTDIR/plugins/zsh-autosuggestions
+VAR=$XDG_CONFIG_HOME/.zsh-plugins/zsh-autosuggestions
+remkdir $VAR
+git clone https://github.com/zsh-users/zsh-autosuggestions $VAR
 
 ## ZSH SYNTAX highlighting
-remkdir $ZDOTDIR/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZDOTDIR/plugins/zsh-syntax-highlighting
+VAR=$XDG_CONFIG_HOME/.zsh-plugins/zsh-syntax-highlighting
+remkdir $VAR
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $VAR
 
 ## TMUX PACKAGE MANAGER
 remkdir $HOME/.tmux
@@ -53,7 +51,9 @@ remkdir $HOME/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
 ## TMUX CONFIG
-link_dotfile $DOTFILES_DIR/.tmux.conf $HOME .tmux.conf
+
+#link_dotfile $DOTFILES_DIR/.tmux.conf $HOME .tmux.conf
+stow -d $DOTFILES_DIR tmux
 
 # TOKYONIGHT
 rm -rf $/HOME/tokyonight.nvim
@@ -63,7 +63,9 @@ git clone https://github.com/folke/tokyonight.nvim.git
 # bat
 # already installed above..
 #
-remkdir $HOME/.config/bat/themes
+stow -d $DOTFILES_DIR bat
+
+#remkdir $HOME/.config/bat/themes
 TN_SUB_THEME=$HOME/tokyonight.nvim/extras/sublime
 BAT_THEMES=$XDG_CONFIG_HOME/bat/themes
 link_dotfile $TN_SUB_THEME/tokyonight_day.tmTheme $BAT_THEMES tokyonight_day.tmTheme
@@ -71,4 +73,6 @@ link_dotfile $TN_SUB_THEME/tokyonight_moon.tmTheme $BAT_THEMES tokyonight_moon.t
 link_dotfile $TN_SUB_THEME/tokyonight_night.tmTheme $BAT_THEMES tokyonight_night.tmTheme
 link_dotfile $TN_SUB_THEME/tokyonight_storm.tmTheme $BAT_THEMES tokyonight_storm.tmTheme
 
-bat cache --build
+#bat cache --build
+
+export PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/var/lib/flatpak/exports/bin:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:~/.local/bin:/opt/nvim-linux64/bin:~/.local/bin:/opt/nvim-linux64/bin
