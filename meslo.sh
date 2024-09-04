5c47ca1fa2a845287e0c0ec352589ed860983131
James Storm <jamesstorm@gmail.com>
2024-09-04 16:23:04 -0400

    all the meslos

--- meslo.sh
+++ meslo.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-fonturl="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Meslo/L/Regular/MesloLGLNerdFontMono-Regular.ttf"
-sudo wget -P /usr/local/share/fonts/ ${fonturl}
-
-fonturl="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Meslo/L/Regular/MesloLGLNerdFontMono-Regular.ttf"
-sudo wget -P /usr/local/share/fonts/ ${fonturl}
-
-fonturl="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Meslo/L/Regular/MesloLGLNerdFontMono-Regular.ttf"
-sudo wget -P /usr/local/share/fonts/ ${fonturl}
-
-fonturl="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Meslo/L/Regular/MesloLGLNerdFontMono-Regular.ttf"
-sudo wget -P /usr/local/share/fonts/ ${fonturl}
+reg_fonturl="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Meslo/L/Regular/MesloLGLNerdFontMono-Regular.ttf"
+bol_fonturl="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Meslo/L/Bold/MesloLGLNerdFontMono-Bold.ttf"
+ita_fonturl="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Meslo/L/Italic/MesloLGLNerdFontMono-Italic.ttf"
+itb_fonturl="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Meslo/L/Bold-Italic/MesloLGLNerdFontMono-BoldItalic.ttf"
+wget -P ~/.local/share/fonts/ ${reg_fonturl}
+wget -P ~/.local/share/fonts/ ${bol_fonturl}
+wget -P ~/.local/share/fonts/ ${ita_fonturl}
+wget -P ~/.local/share/fonts/ ${itb_fonturl}
