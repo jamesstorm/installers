199191090bf9da86ca0f0f72c1e371d550035783
James Storm <jamesstorm@gmail.com>
2024-09-05 08:48:13 -0400

    waybar things

---
+++ hyprland.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+## install pre-requisites for builing hyprland
+yay -S --needed aquamarine gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang hyprcursor hyprwayland-scanner xcb-util-errors hyprutils-git
