ec4cd4eac015278f8c0bd2a1cd85afdc008654a0
James Storm <jamesstorm@gmail.com>
2024-09-15 12:21:13 -0400

    arch scripts for macbook

---
+++ arch/mount_subvolumes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mount -o noatime,compress=lzo,space_cache,subvol=@root /dev/nvme0n1p3 /mnt
+mount --mkdir -o noatime,compress=lzo,subvol=@home /dev/nvme0n1p3 /mnt/home
+mount --mkdir -o noatime,compress=lzo,subvol=@srv /dev/nvme0n1p3 /mnt/srv
+mount --mkdir -o noatime,compress=lzo,subvol=@tmp /dev/nvme0n1p3 /mnt/tmp
+mount --mkdir -o noatime,compress=lzo,subvol=@opt /dev/nvme0n1p3 /mnt/opt
+mount --mkdir -o noatime,compress=lzo,subvol=@.snapshots /dev/nvme0n1p3 /mnt/.snapshots
+mount --mkdir -o nodatacow,subvol=@swap /dev/nvme0n1p3 /mnt/swap
+mount --mkdir -o nodatacow,subvol=@var /dev/nvme0n1p3 /mnt/var
+mount --mkdir /dev/nvmen0n1p1 /mount/boot
