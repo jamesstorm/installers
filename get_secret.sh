ca28cbec8db8314f28d98b1d3f2529cfe81f310f
James Storm <jamesstorm@gmail.com>
2024-09-16 16:53:36 -0400

    can run in any dir

--- get_secret.sh
+++ get_secret.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+DIR=$HOME/installers
 read -sp 'password: ' VAULTPWD
 echo "${VAULTPWD}" >>/tmp/pwd.txt
 
-ansible-vault decrypt ./secrets/secretkey --vault-password-file /tmp/pwd.txt --output /tmp/deleteme
+ansible-vault decrypt $DIR/secrets/secretkey --vault-password-file /tmp/pwd.txt --output /tmp/deleteme
 cat /tmp/deleteme
 rm /tmp/pwd.txt
 rm /tmp/deleteme
