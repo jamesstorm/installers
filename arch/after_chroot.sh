3414c012cb2f70a56ad4219b97662163113f8855
James Storm <jamesstorm@gmail.com>
2024-09-16 13:51:51 -0400

    archie things for macbooks

--- arch/after_chroot.sh
+++ arch/after_chroot.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 
-# creat swapfile and add to fstab
+# Install more things
+
+pacman -S alsa-utils arch-install-scripts archinstall bind bolt brltty btrfs-progs clonezilla cryptsetup darkhttpd ddrescue dhclient dhcpcd diffutils dmidecode dmraid dnsmasq dosfstools e2fsprogs edk2-shell espeakup ethtool exfatprogs f2fs-tools fatresize fsarchiver gnu-netcat gpart gpm gptfdisk grml-zsh-config hdparm hyperv intel-ucode irssi iw jfsutils kitty-terminfo less lftp libfido2 libusb-compat linux-atm linux-firmware-marvell livecd-sounds lsscsi lvm2 lynx man-db man-pages mc mdadm memtest86+ mkinitcpio mkinitcpio-archiso mkinitcpio-nfs-utils modemmanager mtools nano nbd ndisc6 nfs-utils nilfs-utils nmap ntfs-3g nvme-cli open-iscsi open-vm-tools openconnect openssh openvpn partclone parted partimage pcsclite ppp pptpclient pv python-requests qemu-guest-agent refind reflector reiserfsprogs rp-pppoe rsync rxvt-unicode-terminfo screen sdparm sg3_utils smartmontools sof-firmware squashfs-tools sudo syslinux systemd-resolvconf t2strap tcpdump terminus-font testdisk tmux tpm2-tss udftools usb_modeswitch usbmuxd usbutils vim virtualbox-guest-utils-nox vpnc wireless-regdb wireless_tools wpa_supplicant wvdial xfsprogs xl2tpd zsh neovim stow unzip openssh git # creat swapfile and add to fstab
 
 btrfs filesystem mkswapfile --size 4g --uuid clear /swap/swapfile
 swapon /swap/swapfile
--- arch/before_chroot.sh
+++ arch/before_chroot.sh
@@ -57,9 +57,11 @@ mount --mkdir $BOOT_PARTITION /mnt/boot
 # Install base packages
 
 # this is the list from the t2archiso
-pacstrap /mnt alsa-utils apple-t2-audio-config apple-bcm-firmware arch-install-scripts archinstall base bind bolt brltty btrfs-progs clonezilla cryptsetup darkhttpd ddrescue dhclient dhcpcd diffutils dmidecode dmraid dnsmasq dosfstools e2fsprogs edk2-shell efibootmgr espeakup ethtool exfatprogs f2fs-tools fatresize fsarchiver gnu-netcat gpart gpm gptfdisk grml-zsh-config grub hdparm hyperv intel-ucode irssi iw iwd jfsutils kitty-terminfo less lftp libfido2 libusb-compat linux-t2 linux-atm linux-firmware linux-firmware-marvell livecd-sounds lsscsi lvm2 lynx man-db man-pages mc mdadm memtest86+ mkinitcpio mkinitcpio-archiso mkinitcpio-nfs-utils modemmanager mtools nano nbd ndisc6 nfs-utils nilfs-utils nmap ntfs-3g nvme-cli open-iscsi open-vm-tools openconnect openssh openvpn partclone parted partimage pcsclite ppp pptpclient pv python-requests qemu-guest-agent refind reflector reiserfsprogs rp-pppoe rsync rxvt-unicode-terminfo screen sdparm sg3_utils smartmontools sof-firmware squashfs-tools sudo syslinux systemd-resolvconf t2fanrd t2strap tcpdump terminus-font testdisk tiny-dfr tmux tpm2-tss udftools usb_modeswitch usbmuxd usbutils vim virtualbox-guest-utils-nox vpnc wireless-regdb wireless_tools wpa_supplicant wvdial xfsprogs xl2tpd zsh
+
+pacstrap /mnt base linux-t2 linux-t2-headers apple-t2-audio-config apple-bcm-firmware linux-firmware iwd grub efibootmgr tiny-dfr t2fanrd
+
 #my additions
-pacstrap /mnt neovim stow git linux-t2 linux-t2-headers unzip openssh
+pacstrap /mnt neovim stow git openssh
 
 # generate the fstab
 genfstab -U /mnt >>/mnt/etc/fstab
