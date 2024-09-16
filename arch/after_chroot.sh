36fad593ec059e420febe8e45f1c0de880a3bd19
James Storm <jamesstorm@gmail.com>
2024-09-16 15:58:17 -0400

    archie things for macbooks

--- arch/after_chroot.sh
+++ arch/after_chroot.sh
@@ -1,40 +1,35 @@
 #/bin/bash
 
+cd $HOME/installers/arch/
+
+USER=james
+PASSWORD=james
+ROOTPASSWORD=james
+
+source ./pacman.sh
+
+#set the root user password
+echo -e "$ROOTPASSWORD\n$ROOTPASSWORD" | passwd
+
+# create a normal user and make it a sudoer
+useradd -rm -d /home/$USER -s /usr/bin/zsh -p "$(openssl passwd -1 $PASSWORD)" $USER
+echo "$USER ALL=(ALL:ALL) ALL" >>/etc/sudoers
+
 # Install more things
-
-pacman -S alsa-utils arch-install-scripts archinstall bind bolt brltty btrfs-progs clonezilla \
-  cryptsetup darkhttpd ddrescue dhclient dhcpcd diffutils dmidecode dmraid dnsmasq \
-  dosfstools e2fsprogs edk2-shell espeakup ethtool exfatprogs f2fs-tools fatresize \
-  fsarchiver gnu-netcat gpart gpm gptfdisk grml-zsh-config hdparm hyperv intel-ucode \
-  irssi iw jfsutils kitty-terminfo less lftp libfido2 libusb-compat linux-atm \
-  linux-firmware-marvell livecd-sounds lsscsi lvm2 lynx man-db man-pages mc mdadm \
-  memtest86+ mkinitcpio mkinitcpio-archiso mkinitcpio-nfs-utils modemmanager mtools \
-  nano nbd ndisc6 nfs-utils nilfs-utils nmap ntfs-3g nvme-cli open-iscsi open-vm-tools \
-  openconnect openssh openvpn partclone parted partimage pcsclite ppp pptpclient pv \
-  python-requests qemu-guest-agent refind reflector reiserfsprogs rp-pppoe rsync \
-  rxvt-unicode-terminfo screen sdparm sg3_utils smartmontools sof-firmware squashfs-tools \
-  sudo syslinux systemd-resolvconf t2strap tcpdump terminus-font testdisk tmux tpm2-tss \
-  udftools usb_modeswitch usbmuxd usbutils vim virtualbox-guest-utils-nox vpnc \
-  wireless-regdb wireless_tools wpa_supplicant wvdial xfsprogs xl2tpd zsh neovim \
-  stow unzip openssh git
-
-# creat swapfile and add to fstab
-
-pacman -S neovim stow git openssh
 btrfs filesystem mkswapfile --size 4g --uuid clear /swap/swapfile
 swapon /swap/swapfile
 
 echo "" >>/etc/fstab
 echo "/swap/swapfile none swap defaults 0 0" >>/etc/fstab
 
-# set time zone and hwclock
+# set time zone and hwclock a
 
 ln -sf /usr/share/zoneinfo/Canada/Eastern /etc/localtime
 hwclock --systohc
 
 # Generate the locales
 sed -i 's/#en_CA.UTF-8 UTF-8/en_CA.UTF-8 UTF-8/' /etc/locale.gen
-echo "LANG=en_CS.UTF-8" >>/etc/locale.conf
+echo "LANG=en_CA.UTF-8" >>/etc/locale.conf
 locale-gen
 
 # Hostname
@@ -45,12 +40,8 @@ cp hosts /etc/hosts
 
 # User and Root password
 
-echo -e "james\njames" | passwd
-useradd -rm -d /home/james -s /bin/zsh -u 1000 -g 1000 -G sudo -p "$(openssl passwd -1 james)" james
-echo "james ALL=(ALL:ALL) ALL" >>/etc/sudoers
-
 # Apple T2 chip thingy
-sed -i 's/Modules=()/Modules=(apple-bce)/' /etc/mkinitcpio.conf
+sed -i 's/Modules=()/Modules=(apple-bce)/g' /etc/mkinitcpio.conf
 mkinitcpio -P
 
 #enable some services
@@ -74,15 +65,14 @@ systemctl enable systemd-udevd.service
 systemctl enable systemd-userdbd.service
 systemctl enable t2fanrd.service
 systemctl enable tiny-dfr.service # this may fail
-systemctl enable user@0.service .service
+systemctl enable user@0.service
 
 # to make sure DNS works right
-ln -sf ../run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
 # grub things
 sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="quiet splash intel_iommu=on iommu=pt pcie_ports=compat"/g' /etc/default/grub
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 
-sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="quiet splash intel_iommu=on iommu=pt pcie_ports=compat"/g' /etc/default/grub
 echo "DONE!!!! exit and reboot"
