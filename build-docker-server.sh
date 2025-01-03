80ad85c1373986185cc22c9611662973d8377204
James Storm <jamesstorm@gmail.com>
2025-01-03 14:23:52 -0500

    x

---
+++ build-docker-server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Assumptions:
+# - fresh Debain 12 install
+# - a user named james
+#
+#
+#
+#
+# install the things
+# git, gh
+# docker, docker-compose
+
+source ./gh.sh
+
+# get git secrets
+#
+#
+# get the
