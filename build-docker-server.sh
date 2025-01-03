46cfad23affa6ca80eb814b260c612a094abd468
James Storm <jamesstorm@gmail.com>
2025-01-03 15:35:28 -0500

    sdf

--- build-docker-server.sh
+++ build-docker-server.sh
@@ -13,7 +13,6 @@ cd ~
 # install the things
 sudo apt install stow ansible curl build-essential -y
 
-
 git clone https://github.com/jamesstorm/installers
 ./installers/gh-debian.sh
 ./installers/dotfiles.sh
@@ -21,6 +20,7 @@ git clone https://github.com/jamesstorm/installers
 mkdir -p ~/.config
 
 stow -d ~/dotfiles git
+rm ~/.config/nvim/lua/config/keymaps.lua
 stow -d ~/dotfiles nvim
 
 cd ~/installers
@@ -32,4 +32,4 @@ cd ~/installers
 # get git secrets
 #
 #
-# get the
+# get t ahdd e
