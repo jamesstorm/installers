8faf71466df4363147fd82b6320e53ee6244d43a
James Storm <jamesstorm@gmail.com>
2025-01-03 15:07:41 -0500

    fooo

--- neovim-tar.sh
+++ neovim-tar.sh
@@ -3,5 +3,7 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.
 sudo rm -rf /usr/bin/nvim
 sudo rm -rf /opt/nvim-linux64
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
-sudo rm nvim-linux64.tar.gz 
-echo "PATH=/opt/nvim-linux64/bin:$PATH" >> ~/.zshrc
+sudo rm nvim-linux64.tar.gz
+echo "PATH=/opt/nvim-linux64/bin:$PATH" >>~/.zshrc
+echo "PATH=/opt/nvim-linux64/bin:$PATH" >>~/.profile
+source ~/.profile
