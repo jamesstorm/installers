742e57cc93e3434ca8b1898fb8919ea73e622b87
James Storm <jamesstorm@gmail.com>
2025-01-03 20:51:10 -0500

    foo

---
+++ build-docker-server/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Preqrequisites:
+# - fresh Debain 12 install
+# - a user named james
+# - git installed
+# - stow installed
+# - james is in sudoers
+# - installers and dotfiles grabbed from github
+
+./1.sh
+./2.sh
+sudo ./3.sh
