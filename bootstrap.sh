1330c154b8be9aac39479ab9cecc5362d59079fa
James Storm <jamesstorm@gmail.com>
2025-04-26 06:00:06 -0400

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-FUNCTIONS_URL="https://github.com/jamesstorm/installers/master/functions.sh"
+FUNCTIONS_URL="https://raw.githubusercontent.com/jamesstorm/installers/refs/heads/master/functions.sh"
+
+# run as root
+if [ $(id -u) -ne 0 ]; then
+  echo "please run as root"
+  exit 1
+fi
 
 # Download and source it
 . /dev/stdin <<EOF
 $(curl -fsSL "$FUNCTIONS_URL")
 EOF
 
-echo "Hello, World."
-
-
-# Call it
+get_real_user
 get_os
 
-# Now OS_NAME is set
+echo "Hello, $REAL_USER"
 echo "Detected OS: $OS_NAME"
+
+if [ "$OS_NAME" = "debian" ]; then
+	apt update -y
+	apt upgrade -y 
+	apt install -y git gh stow ansible
+elif [ "$OS_NAME" = "arch" ]; then
+	echo "pacman calls not implemented... yet"
+	exit 1
+fi
+
+sudo -u $REAL_USER git clone https://github.com/jamesstorm/installers $HOME_DIR
+sudo -u $REAL_USER git clone https://github.com/jamesstorm/dotfiles $HOME_DIR
